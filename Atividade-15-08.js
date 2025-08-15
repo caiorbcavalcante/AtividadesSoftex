a14be9c1c21960ca01e903e0aadbc5fd4702c9a9
CaioVictor <caiorbcavalcante@gmail.com>
2025-08-15 15:39:17 -0300

    Atividade do dia 15/08 na aula sobre git

--- Atividade-15-08.js
+++ Atividade-15-08.js
@@ -1,8 +1,8 @@
 // FUnção para a checagem de tier de usuário
 function checkTier(userName, userTier) {
-    if (userTier.toLowerCase === "admin") {
+    if (userTier.toLowerCase() === "admin") {
         console.log(`Usuário adminstrador ${userName} entrou!`);
     } else {console.log(`Usuário ${userName} de categoria normal entrou.`);}    
 }
-console.log(checkTier("caio", "normal"));
+console.log(checkTier("caio", "admin"));
 
